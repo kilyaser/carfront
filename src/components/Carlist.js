91c5a80fd9198049f6c4de15b78c15e9da7f7e90
arcadag <piknik0711@yandex.ru>
2023-03-24 21:34:00 +0300

    Founded the basis of the project, created the  Carlist.js  component and implemented a query function to get a list of machines

---
+++ src/components/Carlist.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import { SERVER_URL } from '../constants';
+import { DataGrid } from '@mui/x-data-grid';
+
+function Carlist() {
+    const [cars, setCars] = useState([]);
+    const columns = [
+        {field: 'brand', headerName: 'Brand', width: 200},
+        {field: 'model', headerName: 'Model', width: 200},
+        {field: 'color', headerName: 'Color', width: 200},
+        {field: 'year', headerName: 'Year', width: 200},
+        {field: 'price', headerName: 'Price', width: 200},
+    ];
+
+    useEffect(() => {
+        fetch(SERVER_URL + 'api/cars')
+        .then(response => response.json())
+        .then(data => setCars(data._embedded.cars))
+        .catch(err => console.error(err));
+    }, []);
+    
+    return(
+        <div style={{height: 500, width: '100%'}}>
+            <DataGrid
+            rows={cars}
+            columns={columns}
+            getRowId={row => row._links.self.href}/>
+        </div>
+    );
+}
+
+export default Carlist;
