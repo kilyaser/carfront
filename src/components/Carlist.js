aafba510e23463ac11494362b0bb061d852f7d87
arcadag <piknik0711@yandex.ru>
2023-03-26 19:51:58 +0300

    implemented all CRUD functionalities

--- src/components/Carlist.js
+++ src/components/Carlist.js
@@ -2,18 +2,26 @@ import React, { useEffect, useState } from 'react';
 import { SERVER_URL} from '../constants';
 import { DataGrid } from '@mui/x-data-grid';
 import { Snackbar } from '@mui/material';
+import AddCar from './AddCar';
+import EditCar from './EditCar';
 
 
 
 function Carlist() {
     const [cars, setCars] = useState([]);
     const columns = [
-        {field: 'brand', headerName: 'Brand', width: 200},
-        {field: 'model', headerName: 'Model', width: 200},
-        {field: 'color', headerName: 'Color', width: 200},
-        {field: 'year', headerName: 'Year', width: 200},
-        {field: 'price', headerName: 'Price', width: 200},
-        {field: '_link.self.href', 
+        {field: 'brand', headerName: 'Brand', width: 150},
+        {field: 'model', headerName: 'Model', width: 150},
+        {field: 'color', headerName: 'Color', width: 150},
+        {field: 'year', headerName: 'Year', width: 150},
+        {field: 'price', headerName: 'Price', width: 150},
+        {field: '_links.car.href',
+         headerName: '',
+         sortable: false,
+         filterable: false,
+         renderCell: row => <EditCar data={row} updateCar={updateCar}/>},
+
+        {field: '_links.self.href', 
          headerName: '', 
          sortable: false, 
          filterable: false,
@@ -32,7 +40,6 @@ function Carlist() {
         .then(data => setCars(data._embedded.cars))
         .catch(err => console.error(err));
     }
-
     const onDelClick = (url) => {
         if (window.confirm("Are you sure to delete?")) {
              fetch(url, {method: 'DELETE'})
@@ -49,21 +56,51 @@ function Carlist() {
         }
        
     }
-
-    
+    const addCar = (car) => {
+        fetch(SERVER_URL + 'api/cars', {
+            method: 'POST',
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify(car)
+        })
+        .then(response => {
+            if (response.ok) {
+                fetchCars();
+            } else {
+                alert('Something went wrong!');
+            }
+        }).catch(err => console.error(err))
+    }
+    const updateCar = (car, link) => {
+        fetch(link, {
+            method: 'PUT',
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify(car)
+        })
+        .then(response => {
+            if (response.ok) {
+                fetchCars();
+            } else {
+                alert('Something went wrong!');
+            }
+        })
+        .catch(err => console.error(err))
+    }    
     
     return(
-        <div style={{height: 500, width: '100%'}}>
-            <DataGrid
-                rows={cars}
-                columns={columns}
-                getRowId={row => row._links.self.href}/>
-            <Snackbar 
-                open={open}
-                autoHideDuration={2000}
-                onClose={() => setOpen(false)}
-                message='Car deleted'/>
-        </div>
+        <React.Fragment>
+            <AddCar addCar={addCar}/>
+             <div style={{height: 500, width: '100%'}}>
+                <DataGrid
+                    rows={cars}
+                    columns={columns}
+                    getRowId={row => row._links.self.href}/>
+                <Snackbar 
+                    open={open}
+                    autoHideDuration={2000}
+                    onClose={() => setOpen(false)}
+                    message='Car deleted'/>
+            </div>
+        </React.Fragment>
     );
 }
 
