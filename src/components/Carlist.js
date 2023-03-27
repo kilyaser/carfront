51e9ced990dc0ee589c7fd8d8b2b7a6d5724ff4d
arcadag <piknik0711@yandex.ru>
2023-03-27 23:13:18 +0300

    implemented the authorization function

--- src/components/Carlist.js
+++ src/components/Carlist.js
@@ -40,14 +40,20 @@ function Carlist() {
     }, []);
 
     const fetchCars = () => {
-        fetch(SERVER_URL + 'api/cars')
+        const token = sessionStorage.getItem("jwt");
+
+        fetch(SERVER_URL + 'api/cars', {headers: {'Authorization': token}})
         .then(response => response.json())
         .then(data => setCars(data._embedded.cars))
         .catch(err => console.error(err));
     }
     const onDelClick = (url) => {
         if (window.confirm("Are you sure to delete?")) {
-             fetch(url, {method: 'DELETE'})
+            const token = sessionStorage.getItem("jwt");
+             fetch(url, {
+                method: 'DELETE', 
+                headers: {'Authorization': token}
+            })
             .then(response => {
                 if(response.ok) {
                     fetchCars();
@@ -62,9 +68,14 @@ function Carlist() {
        
     }
     const addCar = (car) => {
+        const token = sessionStorage.getItem("jwt");
+
         fetch(SERVER_URL + 'api/cars', {
             method: 'POST',
-            headers: {'Content-Type':'application/json'},
+            headers: {
+                'Content-Type':'application/json',
+                'Authorization': token
+            },
             body: JSON.stringify(car)
         })
         .then(response => {
@@ -76,9 +87,14 @@ function Carlist() {
         }).catch(err => console.error(err))
     }
     const updateCar = (car, link) => {
+        const token = sessionStorage.getItem("jwt");
+
         fetch(link, {
             method: 'PUT',
-            headers: {'Content-Type':'application/json'},
+            headers: {
+                'Content-Type':'application/json',
+                'Authorization': token
+            },
             body: JSON.stringify(car)
         })
         .then(response => {
